dd5d79b7dfd06ca0d05f0a246e93a3495df80f80
Stevie <yzm629@sina.cn>
2023-11-04 05:03:09 +0800

    -

--- server/utils/transformer.ts
+++ server/utils/transformer.ts
@@ -1,10 +1,27 @@
 import { uneval } from 'devalue';
 import superjson from 'superjson';
+import { unpack, pack } from 'msgpackr';
+import type {DataTransformerOptions} from "@trpc/server";
 
-export default {
-  input: superjson,
-  output: {
-    serialize: uneval,
-    deserialize: (str) => (0, eval)(`(${str})`)
+const msgpacAndSuperjson = {
+  serialize(object: any): any {
+    const compressed = pack(superjson.serialize(object))
+    return btoa(String.fromCharCode(...Array.from(compressed)))
   },
+  deserialize(object: any): any {
+    const notDecompressed = Uint8Array.from(atob(object), c => c.charCodeAt(0))
+    return superjson.deserialize(unpack(notDecompressed))
+  }
+}
+
+const evalHandler = {
+  serialize: uneval,
+  deserialize: (str) => (0, eval)(`(${str})`)
+}
+
+export default <DataTransformerOptions>{
+  input: superjson,
+  // input: msgpacAndSuperjson,
+  output: evalHandler,
+  // output: msgpackr,
 };
