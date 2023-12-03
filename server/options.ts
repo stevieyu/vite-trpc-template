591931d424c3e99fa0ffc13a29038b7b79e0b773
Stevie <yzm629@sina.cn>
2023-12-03 19:01:25 +0800

    -

--- server/options.ts
+++ server/options.ts
@@ -1,17 +1,18 @@
 
 // import { db } from './db';
 import { publicProcedure, router } from './trpc';
+import { wrap } from '@decs/typeschema';
 import { object, string, optional, array, union, number, date, any } from 'valibot'
 // import type {NodeHTTPCreateContextFn, NodeHTTPRequest, NodeHTTPResponse} from "@trpc/server/dist/adapters/node-http";
 
 const appRouter = router({
   users: publicProcedure
-    .input(object({
+    .input(wrap(object({
       str: optional(string()),
       arr: optional(array(union([string(), number()]))),
       date: optional(date()),
       test: optional(any()),
-    }))
+    })))
     .query(async (opts) => {
       const { input } = opts
       console.log(`date:`, input.date?.getFullYear());
